e5864fc3d2ec5bf6dbe3c817430c6120e33969c2
esirango <esirango23@gmail.com>
2025-09-02 12:07:30 +0330

    add script for navbar in scroll time

---
+++ js/navbar-scroll.js
@@ -0,0 +1,14 @@
+let lastScrollTop = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+
+    if (scrollTop > lastScrollTop) {
+        header.classList.add("hide");
+    } else if (scrollTop === 0) {
+        header.classList.remove("hide");
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
